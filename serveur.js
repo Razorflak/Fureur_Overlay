a098bec835617063eb8da4062081e5bcde00b6a2
Tanguy J <tanguyj35@gmail.com>
2020-11-13 15:30:14 +0100

    Overlay

--- serveur.js
+++ serveur.js
@@ -6,9 +6,15 @@ var request = require('request');
 var serveIndex = require('serve-index');
 var http = require('http').Server(app);
 app.use(express.static(__dirname + "/"))
-app.use('/', serveIndex(__dirname + '/'));
+app.get("*", (req, res) => {
+	res.sendFile("preview.html", { root: __dirname }, (err) => {
+	  res.end();
+   
+	  if (err) throw err;
+	});
+   });
 
-const port = 6666;
+const port = 3000;
 http.listen(port, () => {
     console.log(`Server listening at ${port}`);
 });
