a8c908216991cf25ee98b0d9b3b7d3f0a8d9656e
unknown <razorflak@gmail.com>
2021-07-09 00:05:46 +0200

    V2

--- IntroSolo/main.js
+++ IntroSolo/main.js
@@ -1,26 +1,24 @@
 function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+		vars[key] = value;
+	});
+	return vars;
 }
 
 var isStreamerReady = false;
 var vidNode;
 var indexVideo;
 
-
-function postChargement(){
+function postChargement() {
 	var vars = getUrlVars();
 	//Mise en place du texte principal
-	
+
 	var pseudo = vars.pseudo;
-	document.getElementById("textWait").innerHTML = pseudo + " arrive ! <br /> Ne bougez pas !"
-	
+	document.getElementById("textWait").innerHTML = pseudo + " arrive ! <br /> Ne bougez pas !";
+
 	var videoOn = vars.video;
-	if(videoOn != '0'){
-		
+	if (videoOn != "0") {
 		//Chargement du XML avec les videos
 		var Connect = new XMLHttpRequest();
 		var choosenRep = "";
@@ -28,66 +26,54 @@ function postChargement(){
 		Connect.open("GET", "videos.xml", false);
 		Connect.setRequestHeader("Content-Type", "text/xml");
 		Connect.send(null);
-		
+
 		//Lecture du xml
 		var TheDocument = Connect.responseXML;
 		var repVideos = TheDocument.childNodes[0];
-		if(typeof choosenRep === "undefined" || choosenRep == "")
-		{
+		if (typeof choosenRep === "undefined" || choosenRep == "") {
 			// TODO A tester !
-			vidNode = repVideos.getElementsByTagName("video"); 
-		}
-		else
-		{
-			for (var i = 0; i < repVideos.children.length; i++)
-			{
+			vidNode = repVideos.getElementsByTagName("video");
+		} else {
+			for (var i = 0; i < repVideos.children.length; i++) {
 				var repNode = repVideos.children[i];
-				var rep = repNode.getAttribute('rep');
-				if(rep == choosenRep)
-				{	
+				var rep = repNode.getAttribute("rep");
+				if (rep == choosenRep) {
 					//Récupération du bon noeud
 					vidNode = repNode.children;
 				}
 			}
 		}
-		
-		
+
 		//On lance la première vidéo
-		var videoLoader = document.getElementById('videoPlayer');
-		vidNode = shuffle(vidNode);
-		indexVideo = 0;
-		var typeVid = vidNode[indexVideo].getAttribute('type');
-		var pathVid = vidNode[indexVideo].getAttribute('path');
+		var videoLoader = document.getElementById("videoPlayer");
+		indexVideo = generateRandom(0, vidNode.length - 1, null);
+		var typeVid = vidNode[indexVideo].getAttribute("type");
+		var pathVid = vidNode[indexVideo].getAttribute("path");
 		videoLoader.type = typeVid;
 		videoLoader.src = pathVid;
-		
+
 		var audioLvl = vars.audioLvl;
-		if(audioLvl != null){
-			videoLoader.volume = audioLvl/100;
-		}else{
+		if (audioLvl != null) {
+			videoLoader.volume = audioLvl / 100;
+		} else {
 			videoLoader.volume = 0.05;
 		}
-		
+
 		videoLoader.addEventListener("loadedmetadata", onMetaDateVideoLoad);
 		videoLoader.onended = onEndVid;
 		videoLoader.onclick = onVideoLoaderClick;
 	}
 }
 
-function onEndVid(){
-	var videoLoader = document.getElementById('videoPlayer');
+function onEndVid() {
+	var videoLoader = document.getElementById("videoPlayer");
 	var sourceNextVideo = document.createElement("source");
-	indexVideo++;
-	if(indexVideo == vidNode.length)
-	{
-		//Fin de playlist sélectionné, on repart du début
-		indexVideo = 0;
-	}
-	
-	var typeVid = vidNode[indexVideo].getAttribute('type');
-	var pathVid = vidNode[indexVideo].getAttribute('path');
+	indexVideo = generateRandom(0, vidNode.length - 1, null);
 
-	if(isStreamerReady){
+	var typeVid = vidNode[indexVideo].getAttribute("type");
+	var pathVid = vidNode[indexVideo].getAttribute("path");
+
+	if (isStreamerReady) {
 		//Le streamer à cliqué sur la page, on lance la vidéo d'intro Fureur en FullScreen
 		document.body.appendChild(videoLoader);
 		videoLoader.style.width = "1920px";
@@ -95,31 +81,23 @@ function onEndVid(){
 		videoLoader.style.zIndex = "10";
 		videoLoader.type = "video/webm";
 		videoLoader.src = "../video/intro/intro.webm";
-	}else{
+	} else {
 		videoLoader.type = typeVid;
 		videoLoader.src = pathVid;
 	}
 }
-function onMetaDateVideoLoad(){
-	
-}
+function onMetaDateVideoLoad() {}
 
-function onVideoLoaderClick(){
+function onVideoLoaderClick() {
 	isStreamerReady = true;
 }
 
-function onFrameXmlLoad(){
+function onFrameXmlLoad() {
 	var frameXml = document.getElementById("frameXml");
 	var t = "1";
 }
 
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
+function generateRandom(min, max, execept) {
+	var num = Math.floor(Math.random() * (max - min + 1)) + min;
+	return num === execept ? generateRandom(min, max) : num;
 }
